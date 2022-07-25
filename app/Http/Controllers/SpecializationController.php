a57f113a3c7b0857ba3fad8d038bd4d8564e3ece
محمد محمد <softech.mohammadthedark333@gmail.com>
2022-07-26 00:40:52 +0300

    create Teacher - Gender - Specialization

---
+++ app/Http/Controllers/SpecializationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\specialization;
+use Illuminate\Http\Request;
+
+class SpecializationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\specialization  $specialization
+     * @return \Illuminate\Http\Response
+     */
+    public function show(specialization $specialization)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\specialization  $specialization
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(specialization $specialization)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\specialization  $specialization
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, specialization $specialization)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\specialization  $specialization
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(specialization $specialization)
+    {
+        //
+    }
+}
