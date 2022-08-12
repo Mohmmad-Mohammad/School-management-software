aee111c563268519006b9f5304175905b3bf5b3f
محمد محمد <softech.mohammadthedark333@gmail.com>
2022-08-12 13:13:06 +0300

    add-Attendances

--- routes/web.php
+++ routes/web.php
@@ -22,26 +22,10 @@ Route::get('/', function () {
     return view('auth.login');
 
 });
+
 });
-
-Route::group(['prefix' => LaravelLocalization::setLocale(), 'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]], function(){ //...
-Route::get('/dashboard', 'HomeController@index')->name('home');
-
-        Route::resource('grades', 'GradeController');
-
-
-    Route::resource('Classroom', 'ClassroomController');
-    Route::post('delete_all','ClassroomController@delete_all')->name('delete_all');
-    Route::post('Filter_Classes', 'ClassroomController@Filter_Classes')->name('Filter_Classes');
-
-
-
-    Route::resource('Section', 'SectionController');
-    Route::get('/classes/{id}', 'SectionController@getclasses');
-
-    Route::view('add_parent', 'livewire.show_Form');
-
-
+        Route::group(['prefix' => LaravelLocalization::setLocale(), 'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]], function(){ //...
+        Route::get('/dashboard', 'HomeController@index')->name('home');
         Route::resource('Teachers', 'TeacherController');
         Route::resource('Students', 'StudentController');
         Route::resource('online_classes', 'OnlineClasseController');
@@ -53,14 +37,20 @@ Route::get('/dashboard', 'HomeController@index')->name('home');
         Route::resource('ProcessingFee', 'ProcessingFeeController');
         Route::resource('Payment_students', 'PaymentController');
         Route::resource('Attendance', 'AttendanceController');
+        Route::resource('grades', 'GradeController');
+        Route::resource('Classroom', 'ClassroomController');
+        Route::resource('Section', 'SectionController');
+        Route::post('delete_all','ClassroomController@delete_all')->name('delete_all');
+        Route::post('Filter_Classes', 'ClassroomController@Filter_Classes')->name('Filter_Classes');
+        Route::post('Upload_attachment', 'StudentController@Upload_attachment')->name('Upload_attachment');
+        Route::post('Delete_attachment', 'StudentController@Delete_attachment')->name('Delete_attachment');
+        Route::post('Delete_attachment', 'StudentController@Delete_attachment')->name('Delete_attachment');
+        Route::post('Promotion.edit/{id}', 'PromotionController@edit')->name('Promotion.edit');
+        Route::view('add_parent', 'livewire.show_Form');
+        Route::get('/classes/{id}', 'SectionController@getclasses');
         Route::get('/Get_classrooms/{id}', 'StudentController@Get_classrooms');
         Route::get('/Get_Sections/{id}', 'StudentController@Get_Sections');
-        Route::post('Upload_attachment', 'StudentController@Upload_attachment')->name('Upload_attachment');
-    Route::post('Delete_attachment', 'StudentController@Delete_attachment')->name('Delete_attachment');
-    Route::post('Delete_attachment', 'StudentController@Delete_attachment')->name('Delete_attachment');
-    Route::post('Promotion.edit/{id}', 'PromotionController@edit')->name('Promotion.edit');
-
 
 });
-Route::get('Show_attachment/{studentsname}/{filename}', 'StudentController@Show_attachment')->name('Show_attachment');
-Route::get('Download_attachment/{studentsname}/{filename}', 'StudentController@Download_attachment')->name('Download_attachment');
+        Route::get('Show_attachment/{studentsname}/{filename}', 'StudentController@Show_attachment')->name('Show_attachment');
+        Route::get('Download_attachment/{studentsname}/{filename}', 'StudentController@Download_attachment')->name('Download_attachment');
